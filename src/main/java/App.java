3b7d9b15bc9966cd42a762e265438cffc0289c15
Joshua Logan and Noah Kittleson <joshua+noah@>
2016-05-03 13:22:12 -0700

    updated project using SQL and a DB

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,8 +11,56 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-
+      model.put("categories", Category.all());
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+
+      model.put("category", category);
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/categories/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+      String description = request.queryParams("task");
+      Task newTask = new Task (description, category.getId());
+      newTask.save();
+
+      model.put("category", category);
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/categories", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String name = request.queryParams("name");
+      Category newCategory = new Category(name);
+      newCategory.save();
+
+      model.put("categories", Category.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/tasks", (request, response) -> {
+      HashMap<String, Object> model =  new HashMap<String, Object>();
+
+      Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
+
+      String description = request.queryParams("description");
+
+      Task newTask = new Task(description, category.getId());
+      newTask.save();
+
+      model.put("category", category);
+      model.put("template", "templates/category-tasks-success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,13 @@
+
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.sql2o.*;
+import org.junit.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -17,9 +20,46 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Before
+    public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteTasksQuery = "DELETE FROM tasks *;";
+      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(deleteTasksQuery).executeUpdate();
+      con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
+
+
   @Test
   public void rootTest() {
+    goTo("http://localhost:4567");
+    assertThat(pageSource()).contains("to do list");
+  }
 
+  @Test
+  public void addCategoryAndCheckForIt() {
+    Category testCategory = new Category ("Lawncare");
+    testCategory.save();
+    String categoryPath = String.format("http://localhost:4567/categories/%d", testCategory.getId());
+    goTo(categoryPath);
+    assertThat(pageSource()).contains("Lawncare");
+  }
+
+  @Test
+  public void addTaskToCategory() {
+    goTo("http://localhost:4567");
+    fill("#name").with("Lawncare");
+    submit(".btn");
+    click("a", withText("Lawncare"));
+    fill("#task").with("Mow the lawn");
+    submit(".btn");
+    assertThat(pageSource()).contains("Mow the lawn");
   }
 
 }
